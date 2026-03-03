98d41d0a3cbfd77428aafd1bdbdfb17aee79425d
Shriram Rajeshwar Nawathe <shrinawathe@gmail.com>
2026-03-03 17:09:06 +0530

    screens folder created and about.js file created and inported in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 // import logo from './logo.svg';
 import './App.css';
+// importing the about function from screens
+import About from "./screens/about.js";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  // return (
+  //   <h1>Hello World</h1>
+  // );
+
+  //import function from screens
+  return <About />;
 }
 
 export default App;
+
+//1
+//Basic React App structure-Done Removerd unnecessary code and added boilerplate code to know we are using react and passing function from screens to app.js
+//screens and components created
+
+// 2 go about.js in screens folder
+
+//3
+//return <About />
+// imported About from screens so it can visibale in browser
---
+++ src/screens/about.js
@@ -0,0 +1,11 @@
+//Boiler code in js to know js were using react and passing function
+//2 file creatted in screens in side screens about file created
+// imported tp app.js and rendered in app.js
+
+import React from 'react';
+
+function About() {
+    return <h1>About Us</h1>;
+}
+
+export default About;
